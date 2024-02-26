7c03b2c08acee4f54e349645e630d8a609dbe243
Mohamed Kamal <ku18mz@gmail.com>
2024-02-26 21:59:50 +0200

    Add CORS support to Flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,18 @@
 """api module main"""
 from models import storage
 from flask import Flask
+from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
+
+CORS(app, resources={r"/*": {"origins": host}})
 
 
 @app.teardown_appcontext
@@ -26,8 +32,4 @@ if __name__ == "__main__":
     """
     run the app with the host and port from the environment or default values
     """
-    host = '0.0.0.0' if getenv('HBNB_API_HOST') is None else getenv(
-        'HBNB_API_HOST')
-    port = 5000 if getenv('HBNB_API_PORT') is None else int(getenv(
-        'HBNB_API_PORT'))
     app.run(host=host, port=port, threaded=True)
