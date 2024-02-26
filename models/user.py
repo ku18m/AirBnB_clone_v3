c54563a8d62fe4af1c1ede1eb4623f3f9ce16935
Mohamed Kamal <ku18mz@gmail.com>
2024-02-27 01:24:10 +0200

    Refactor password handling in User model to be md5 encrypted in both db and fs, used synonym to avoid overloading sqlalchemy methods

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, synonym
 
 
 class User(BaseModel, Base):
@@ -13,17 +13,34 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column('password', String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+
+    @property
+    def password(self):
+        """getter for name"""
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        import hashlib
+        """setter for password"""
+        if password:
+            password = hashlib.md5(password.encode()).hexdigest()
+        self._password = password
+
+    if models.storage_t == 'db':
+        password = synonym('_password', descriptor=password)
